6169d0b19a0d3a28d4741059ebc30b6647641172
amkolotov <amkolotov@mail.ru>
2020-11-14 14:31:29 +0500

    lesson_01

--- project/js/main.js
+++ project/js/main.js
@@ -1,22 +1,78 @@
-const products = [
-    {id: 1, title: 'Notebook', price: 20000},
-    {id: 2, title: 'Mouse', price: 1500},
-    {id: 3, title: 'Keyboard', price: 5000},
-    {id: 4, title: 'Gamepad', price: 4500},
-];
+class ProductList {
+    #goods;
+    #allProducts;
 
-const renderProduct = (title, price) => {
-    return `<div class="product-item row pt-4">
-                <div class="col"><h3 class="col">${title}</h3></div>
-                <div class="col"><p class="col">${price}</p></div>
-                <div class="col"><button class="by-btn btn btn-secondary">Добавить в корзину</button></div>
-              </div>`;
-};
-
-const catalogInit = (list=[{title: 'Нет продуктов', price: 0}]) => {
-    let div = document.querySelector('.products');
-    for (let i = 0; i < list.length; i++) {
-        div.innerHTML += renderProduct(list[i].title, list[i].price);
+    constructor(container = '.products') {
+        this.container = container;
+        this.#goods = [];
+        this.#allProducts = [];
+        this.#fetchGoods();
+        this.#render();
     }
-};
-catalogInit(products);
+
+    #fetchGoods() {
+        this.#goods = [
+            {id: 1, title: 'Notebook', price: 20000},
+            {id: 2, title: 'Mouse', price: 1500},
+            {id: 3, title: 'Keyboard', price: 5000},
+            {id: 4, title: 'Gamepad', price: 4500},
+        ];
+    }
+
+    #render() {
+        const container = document.querySelector(this.container);
+
+        for (let product of this.#goods) {
+            const productObject = new ProductItem(product);
+
+            this.#allProducts.push(productObject);
+
+            container.insertAdjacentHTML('beforeend', productObject.getHTMLString());
+        }
+    }
+}
+
+class ProductItem {
+    constructor(product, img = 'https://placehold.it/200x150') {
+        this.title = product.title;
+        this.id = product.id;
+        this.price = product.price;
+        this.img = img;
+    }
+
+    getHTMLString() {
+        return `<div class="product-item" data-id="${this.id}">
+                      <img src="${this.img}" alt="Some img">
+                      <div class="desc">
+                          <h3>${this.title}</h3>
+                          <p>${this.price} \u20bd</p>
+                          <button class="buy-btn">Купить</button>
+                      </div>
+                  </div>`;
+    }
+}
+
+const productList = new ProductList();
+
+// const products = [
+//   {id: 1, title: 'Notebook', price: 20000},
+//   {id: 2, title: 'Mouse', price: 1500},
+//   {id: 3, title: 'Keyboard', price: 5000},
+//   {id: 4, title: 'Gamepad', price: 4500},
+// ];
+//
+// const renderProduct = (item, img='https://placehold.it/200x150') => `<div class="product-item" data-id="${this.id}">
+//               <img src="${img}" alt="Some img">
+//               <div class="desc">
+//                   <h3>${item.title}</h3>
+//                   <p>${item.price} \u20bd</p>
+//                   <button class="buy-btn">Купить</button>
+//               </div>
+//           </div>`;
+//
+// const renderProducts = list => {
+//     document.querySelector('.products').insertAdjacentHTML('beforeend', list.map(item => renderProduct(item)).join(''));
+// };
+//
+// renderProducts(products);
+
