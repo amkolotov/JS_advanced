74443adad092907dc4b8e548961c8f6f12b65ed4
amkolotov <amkolotov@mail.ru>
2020-11-29 10:50:52 +0500

    homework_05_final

--- project/js/main.js
+++ project/js/main.js
@@ -3,67 +3,19 @@ const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-a
 const app = new Vue({
     el: '#app',
     data: {
-        catalogUrl: '/catalogData.json',
-        products: [],
-        imgCatalog: 'https://placehold.it/200x150',
-        searchLine: '',
-        isVisibleCard: false
+        userSearch: '',
     },
     methods: {
-        getJson(url) {
+        getJson(url){
             return fetch(url)
                 .then(result => result.json())
                 .catch(error => {
                     console.log(error);
                 })
         },
-        addProduct(product) {
-            console.log(product.id_product);
-        },
-        filterGoods() {
-            console.log(this.searchLine)
-            const regexp = new RegExp(this.searchLine, 'i');
-            let searchProduct = this.products.filter(product => regexp.test(product.product_name));
-            this.products.forEach (product => {
-                if (!searchProduct.includes(product)) {
-                    product.visible = false;
-                } else {
-                    product.visible = true;
-                }
-            })
-        },
-        changeVisibleBasket() {
-            this.isVisibleCard ? this.isVisibleCard = false : this.isVisibleCard = true;
-        }
-    },
-    beforeCreate() {
-
-    },
-    created() {
-        this.getJson(`${API + this.catalogUrl}`)
-            .then(data => {
-                for (let el of data) {
-                    el.visible = true
-                    this.products.push(el);
-                }
-            });
-    },
-    beforeMount() {
-
     },
     mounted() {
-
-    },
-    beforeUpdate() {
-
-    },
-    updated() {
-
-    },
-    beforeDestroy() {
-
-    },
-    destroyed() {
-
-    },
+        console.log(this);
+    }
 });
+
