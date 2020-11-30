447e13a508e612c41748a02a64dcb47490765d01
amkolotov <amkolotov@mail.ru>
2020-11-30 12:40:18 +0500

    homework_06

--- project/js/main.js
+++ project/js/main.js
@@ -2,17 +2,18 @@ const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-a
 
 const app = new Vue({
     el: '#app',
-    data: {
-        userSearch: '',
-    },
+
     methods: {
         getJson(url){
             return fetch(url)
                 .then(result => result.json())
                 .catch(error => {
-                    console.log(error);
+                    this.$refs.error.showError(error);
                 })
         },
+        filterProducts(message) {
+            this.$refs.products.filter(message);
+        }
     },
     mounted() {
         console.log(this);
